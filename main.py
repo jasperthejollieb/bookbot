13e0af192c75b39685fae3e8f218d01c76eb1375
jasperthejollieb <jasperthejollieb@gmail.com>
2025-06-28 03:10:43 +0100

    End of the bookbot project. Mixed success. Had to use AI assistance on large amounts of the code for exercise one but did exercise 2 independently. Learned and improved even if assistance required. Need to work without assistance by end of course.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import no_words, no_chars, newd
 
 def get_book_text(path):
@@ -10,15 +11,20 @@ def sort_newd(newd):
 
 
 def main():
-#	print(get_book_text("/home/fors/bookbot/bookbot/books/frankenstein.txt"))
-	no_words(get_book_text("/home/fors/bookbot/bookbot/books/frankenstein.txt"))
-#	print(no_chars(get_book_text("/home/fors/bookbot/bookbot/books/frankenstein.txt")))
-	b = no_chars(get_book_text("/home/fors/bookbot/bookbot/books/frankenstein.txt"))
-	c = newd(b)
-	c.sort(reverse=True, key=sort_newd)
-	print("============ BOOKBOT ============\nAnalyzing book found at books/frankenstein.txt...\n----------- Word Count ----------")
-	print(f"Found {no_words(get_book_text("/home/fors/bookbot/bookbot/books/frankenstein.txt"))} total words\n--------- Character Count -------")
-	for dict in c:
-		print(f"{dict['char']}: {dict['count']}")
-	print("============= END ===============")
+	if len(sys.argv) < 2:
+		print("Usage: python3 main.py <path_to_book>")
+		sys.exit(1)
+	else:
+#		print(get_book_text("/home/fors/bookbot/bookbot/books/frankenstein.txt"))
+		arg2 = sys.argv[1]
+		no_words(get_book_text(arg2))
+#		print(no_chars(get_book_text("/home/fors/bookbot/bookbot/books/frankenstein.txt")))
+		b = no_chars(get_book_text(arg2))
+		c = newd(b)
+		c.sort(reverse=True, key=sort_newd)
+		print(f"============ BOOKBOT ============\nAnalyzing book found at {arg2}...\n----------- Word Count ----------")
+		print(f"Found {no_words(get_book_text("/home/fors/bookbot/bookbot/books/frankenstein.txt"))} total words\n--------- Character Count -------")
+		for dict in c:
+			print(f"{dict['char']}: {dict['count']}")
+		print("============= END ===============")
 main()
